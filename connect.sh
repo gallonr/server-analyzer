5cacacfef1939748f660bff3e59c40ca281138e8
rgallon <regis.gallon@lecnam.net>
2025-10-04 23:49:11 +0200

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -2,8 +2,8 @@
 # Script de connexion rapide au serveur
 # Usage: ./connect.sh
 
-REMOTE_USER="rgallon"
-REMOTE_HOST="195.83.28.108"
+REMOTE_USER="user"
+REMOTE_HOST="A DEFINIR"
 
 echo "🔌 Connexion au serveur ${REMOTE_USER}@${REMOTE_HOST}..."
 echo ""
